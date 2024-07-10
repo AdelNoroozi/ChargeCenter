c5553ecb455f624802c2ba0f87e6e91b7c59d659
adel13no@gmail.com <adel13no@gmail.com>
2024-07-10 23:46:55 +0430

    feat: create salesperson API added

--- chargecenter/users/apis.py
+++ chargecenter/users/apis.py
@@ -1,12 +1,13 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_jwt.permissions import IsSuperUser
 
 from chargecenter.api.mixins import ApiAuthMixin, BasePermissionsMixin
-from chargecenter.users.serializers import AdminInputSerializer
-from chargecenter.users.services import create_admin
+from chargecenter.users.serializers import AdminInputSerializer, SalesPersonInputSerializer
+from chargecenter.users.services import create_admin, create_salesperson
 
 
 class CreateAdminAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
@@ -18,3 +19,14 @@ class CreateAdminAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
     def post(self, request):
         admin_data = create_admin(data=request.data)
         return Response(admin_data, status=status.HTTP_201_CREATED)
+
+
+class CreateSalesPersonAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
+    permissions = {
+        "POST": [IsAdminUser]
+    }
+
+    @extend_schema(tags=['Users:SalesPeople'], request=SalesPersonInputSerializer)
+    def post(self, request):
+        salesperson_data = create_salesperson(data=request.data)
+        return Response(salesperson_data, status=status.HTTP_201_CREATED)
--- chargecenter/users/urls.py
+++ chargecenter/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chargecenter.users.apis import CreateAdminAPI
+from chargecenter.users.apis import CreateAdminAPI, CreateSalesPersonAPI
 
 urlpatterns = [
-    path('add-admin/', CreateAdminAPI.as_view(), name="add_admin")
+    path('add-admin/', CreateAdminAPI.as_view(), name="add_admin"),
+    path('add-salesperson/', CreateSalesPersonAPI.as_view(), name="add_salesperson")
 ]
