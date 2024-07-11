7da980e2d436dd803793c6d2380e317d506cbad9
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 17:18:18 +0430

    feat: update salesperson balance service added

--- chargecenter/users/services/__init__.py
+++ chargecenter/users/services/__init__.py
@@ -1,2 +1,3 @@
 from chargecenter.users.services.admin import create_admin
 from chargecenter.users.services.salesperson import create_salesperson
+from chargecenter.users.services.salesperson import update_salesperson_balance
