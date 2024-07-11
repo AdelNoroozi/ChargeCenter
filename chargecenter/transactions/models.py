ed7d77db685eb92d7cb2ab2f9bfb736c20de8bf1
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 11:25:57 +0430

    feat: balance & charge transaction models added

--- chargecenter/transactions/models.py
+++ chargecenter/transactions/models.py
@@ -1,10 +1,12 @@
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 from chargecenter.common.models import TimeStampedBaseModel
-from chargecenter.users.models import SalesPerson
+from chargecenter.users.models import SalesPerson, BaseUser
 
 
 class Transaction(TimeStampedBaseModel):
@@ -38,3 +40,42 @@ class Transaction(TimeStampedBaseModel):
             models.Index(fields=["salesperson"]),
             models.Index(fields=["amount"])
         ]
+
+
+class ChargeTransaction(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    transaction_obj = models.OneToOneField(Transaction, on_delete=models.RESTRICT, related_name="concrete_charge_obj",
+                                           verbose_name=_("transaction_obj"))
+
+    """the phone number value is saved as str instead of FK to avoid data loss after deletion of phone number objects"""
+    phone_number = PhoneNumberField(verbose_name=_("number"))
+
+    def __str__(self):
+        return f"{self.transaction_obj.amount} for {self.phone_number} at {self.transaction_obj.created_at}"
+
+    class Meta:
+        verbose_name = _("Charge Transaction")
+        verbose_name_plural = _("Charge Transactions")
+
+
+class BalanceTransaction(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    transaction_obj = models.OneToOneField(Transaction, on_delete=models.RESTRICT, related_name="concrete_balance_obj",
+                                           verbose_name=_("transaction_obj"))
+    is_confirmed = models.BooleanField(default=False, verbose_name=_("is confirmed"))
+    confirmed_by = models.ForeignKey(BaseUser, on_delete=models.RESTRICT, related_name="confirmed_transactions",
+                                     blank=True, null=True, verbose_name=_("confirmed by"))
+
+    def clean(self):
+        if not self.confirmed_by.is_admin:
+            raise ValidationError("only staff users can be referred as balance transactions' verifiers")
+
+        super().clean()
+
+    def __str__(self):
+        return f"{self.transaction_obj.amount} for {self.transaction_obj.salesperson.first_name} " \
+               f"{self.transaction_obj.salesperson.last_name} at {self.transaction_obj.created_at}"
+
+    class Meta:
+        verbose_name = _("Balance Transaction")
+        verbose_name_plural = _("Balance Transactions")
