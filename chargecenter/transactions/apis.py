2e7f439a53e55571a62c61871df30681157f0622
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 17:37:11 +0430

    feat: confirm balance transaction API added

--- chargecenter/transactions/apis.py
+++ chargecenter/transactions/apis.py
@@ -1,20 +1,33 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from chargecenter.api.mixins import ApiAuthMixin, BasePermissionsMixin
 from chargecenter.authentication.permissions import IsSalesPerson
-from chargecenter.transactions.serializers import IncreaseBalanceSerializer
+from chargecenter.transactions.serializers import IncreaseBalanceSerializer, ConfirmBalanceTransactionSerializer
 from chargecenter.transactions.services import create_balance_transaction
+from chargecenter.transactions.services.balance import confirm_balance_transaction
 
 
-class CreateBalanceTransaction(ApiAuthMixin, BasePermissionsMixin, APIView):
+class CreateBalanceTransactionAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
     permissions = {
         "POST": [IsSalesPerson]
     }
 
-    @extend_schema(tags=['transactions'], request=IncreaseBalanceSerializer)
+    @extend_schema(tags=['transactions:balance'], request=IncreaseBalanceSerializer)
     def post(self, request):
         admin_data = create_balance_transaction(user=request.user, data=request.data)
         return Response(admin_data, status=status.HTTP_201_CREATED)
+
+
+class ConfirmBalanceTransactionAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
+    permissions = {
+        "PATCH": [IsAdminUser]
+    }
+
+    @extend_schema(tags=['transactions:balance'], request=ConfirmBalanceTransactionSerializer)
+    def patch(self, request):
+        confirm_balance_transaction(admin=request.user, data=request.data)
+        return Response({"message": "done"}, status=status.HTTP_200_OK)
--- chargecenter/transactions/urls.py
+++ chargecenter/transactions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chargecenter.transactions.apis import CreateBalanceTransaction
+from chargecenter.transactions.apis import CreateBalanceTransactionAPI, ConfirmBalanceTransactionAPI
 
 urlpatterns = [
-    path('create-balance/', CreateBalanceTransaction.as_view(), name="create_balance")
+    path('create-balance/', CreateBalanceTransactionAPI.as_view(), name="create_balance"),
+    path('confirm-balance/', ConfirmBalanceTransactionAPI.as_view(), name="confirm_balance"),
 ]
