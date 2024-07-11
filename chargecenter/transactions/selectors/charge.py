dda1a6cb3053fc418e633dc986a51d91763d043c
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 18:49:35 +0430

    fix: create charge selector FK issue resolved

--- chargecenter/transactions/selectors/charge.py
+++ chargecenter/transactions/selectors/charge.py
@@ -2,4 +2,4 @@ from chargecenter.transactions.models import Transaction, ChargeTransaction
 
 
 def create_charge(transaction: Transaction, phone_number: str):
-    return ChargeTransaction.objects.create(transaction=transaction, phone_number=phone_number)
+    return ChargeTransaction.objects.create(transaction_obj=transaction, phone_number=phone_number)
