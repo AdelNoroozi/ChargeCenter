31ac876bcc79dcc1b0d14e21eaf2e4d2037b3338
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 18:40:03 +0430

    refactor: transaction output serializer logic for charges modified

--- chargecenter/transactions/serializers/charge.py
+++ chargecenter/transactions/serializers/charge.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
+from chargecenter.transactions.models import ChargeTransaction
 
-class CreateChargeSerializer(serializers.Serializer):
+
+class ChargeInputSerializer(serializers.Serializer):
     amount = serializers.IntegerField(min_value=1)
     phone_number = serializers.UUIDField()
+
+
+class ChargeOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChargeTransaction
+        exclude = ("transaction_obj",)
--- chargecenter/transactions/serializers/transaction.py
+++ chargecenter/transactions/serializers/transaction.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from chargecenter.transactions.models import Transaction
 from chargecenter.transactions.serializers import BalanceTransactionPrivateSerializer, \
     BalanceTransactionPublicSerializer
+from chargecenter.transactions.serializers.charge import ChargeOutputSerializer
 
 
 class TransactionOutputSerializer(serializers.ModelSerializer):
@@ -14,7 +15,7 @@ class TransactionOutputSerializer(serializers.ModelSerializer):
 
     def get_concrete_obj(self, obj):
         if obj.is_charge:
-            pass
+            return ChargeOutputSerializer(instance=obj.concrete_charge_obj).data
         else:
             if self.context.get("full_access"):
                 return BalanceTransactionPrivateSerializer(instance=obj.concrete_balance_obj).data
