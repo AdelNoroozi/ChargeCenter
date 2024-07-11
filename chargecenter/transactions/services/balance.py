74b680a0c2efd8785c961006959114c41acb9d3d
adel13no@gmail.com <adel13no@gmail.com>
2024-07-11 16:13:16 +0430

    fix: create balance issues resolved

--- chargecenter/transactions/services/balance.py
+++ chargecenter/transactions/services/balance.py
@@ -1,13 +1,15 @@
 from django.db import transaction
 
 from chargecenter.transactions.selectors import create_transaction, create_balance
-from chargecenter.transactions.serializers import TransactionOutputSerializer
+from chargecenter.transactions.serializers import TransactionOutputSerializer, IncreaseBalanceSerializer
 from chargecenter.users.models import BaseUser
 
 
 @transaction.atomic
-def create_balance_transaction(user: BaseUser, amount: int):
+def create_balance_transaction(user: BaseUser, data: dict):
+    serializer = IncreaseBalanceSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
     salesperson = user.salesperson
-    transaction_obj = create_transaction(salesperson=salesperson, amount=amount)
+    transaction_obj = create_transaction(salesperson=salesperson, amount=serializer.data.get("amount"))
     create_balance(transaction=transaction_obj)
-    return TransactionOutputSerializer(instance=transaction_obj).data
+    return TransactionOutputSerializer(instance=transaction_obj, context={"full_access": False}).data
