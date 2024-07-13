2452705e49151f0d1c1870526f5f00e216ab9468
adel13no@gmail.com <adel13no@gmail.com>
2024-07-13 22:34:50 +0430

    refactor: phone number filter is accessible both by id and number now

--- chargecenter/transactions/services/transactions.py
+++ chargecenter/transactions/services/transactions.py
@@ -1,3 +1,5 @@
+from chargecenter.common.validators import is_valid_uuid
+from chargecenter.phones.models import PhoneNumber
 from chargecenter.transactions.filters import TransactionFilter
 from chargecenter.transactions.selectors import get_transactions as get_transactions_selector
 from chargecenter.transactions.serializers import TransactionOutputSerializer
@@ -7,6 +9,11 @@ from chargecenter.users.models import BaseUser
 def get_transactions(user: BaseUser, query_dict: dict):
     transactions = get_transactions_selector(return_all=user.is_admin,
                                              salesperson=user.salesperson if not user.is_admin else None)
-    transactions = TransactionFilter(query_dict, queryset=transactions).qs
+    local_query_dict = query_dict.copy()
+    if is_valid_uuid(val=local_query_dict.get("concrete_charge_obj__phone_number")):
+        phone_number = PhoneNumber.objects.filter(id=local_query_dict.get("concrete_charge_obj__phone_number")).first()
+        if phone_number:
+            local_query_dict["concrete_charge_obj__phone_number"] = phone_number.number
+    transactions = TransactionFilter(local_query_dict, queryset=transactions).qs
     serializer = TransactionOutputSerializer(transactions, many=True, context={"full_access": user.is_admin})
     return serializer.data
